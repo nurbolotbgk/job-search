210f822eb569c8ee488b82191f0f2746c2cf8469
nurbolotbgk <nurbolotbgk@gmail.com>
2026-07-14 18:19:29 +0600

    VacancyController is done.

--- src/main/java/com/example/demo/controller/ResumeController.java
+++ src/main/java/com/example/demo/controller/ResumeController.java
@@ -2,19 +2,41 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Resumes;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/resumes")
+@RequestMapping("resumes")
 public class ResumeController {
     private final List<Resumes> storage = new ArrayList<>();
+
     @GetMapping("resumes")
     public ResponseEntity<List<Resumes>> getAllresumes() {
         return ResponseEntity.ok(storage);
     }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Resumes> searchResumeById(@PathVariable Integer id) {
+        for (Resumes resume : storage) {
+            if (resume.getCategory_id() == id) {
+                return ResponseEntity.ok(resume);
+            }
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("{applicationId}")
+    public ResponseEntity<Resumes> searchAppliacant(@PathVariable Integer applicationId) {
+        for (Resumes resume : storage) {
+            if (resume.getApplication_id() == applicationId) {
+                return ResponseEntity.ok(resume);
+            }
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
 }
--- src/main/java/com/example/demo/controller/VacancyController.java
+++ src/main/java/com/example/demo/controller/VacancyController.java
@@ -1,16 +1,23 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.VacancyDto;
+import com.example.demo.model.Responded_applicants;
 import com.example.demo.service.VacancyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("vacancies")
 @RequiredArgsConstructor
 public class VacancyController {
+
+    private final List<Responded_applicants> responseStorage = new ArrayList<>();
+
     @PostMapping("create")
     public HttpStatus createVacancy(VacancyDto dto) {
         System.out.println(dto.toString());
@@ -27,8 +34,8 @@ public class VacancyController {
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("resumes")
-        public ResponseEntity<void> getAllresumes() {
-        return ResponseEntity.ok().build();
+    @GetMapping("{vacancyId}/responses")
+    public ResponseEntity<List<Responded_applicants>> getResponsesForVacancy(@PathVariable Integer vacancyId) {
+        return ResponseEntity.ok(responseStorage);
     }
     }
