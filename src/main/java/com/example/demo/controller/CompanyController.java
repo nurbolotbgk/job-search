e096b55682257b4c53c462d48fb52ed5b3b1e698
Nurbolot Abilov <nurbolotbgk@gmail.com>
2026-08-18 19:00:48 +0600

    Pages with vacancies sorting by date or responses ready

--- src/main/java/com/example/demo/controller/CompanyController.java
+++ src/main/java/com/example/demo/controller/CompanyController.java
@@ -1,12 +1,15 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.UserDto;
+import com.example.demo.dto.VacancyDto;
 import com.example.demo.service.UserService;
+import com.example.demo.service.VacancyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class CompanyController {
 
     private final UserService userService;
+    private final VacancyService vacancyService;
 
     @GetMapping
     public String getCompanies(@RequestParam(defaultValue = "0") int page, Model model) {
@@ -29,5 +33,24 @@ public class CompanyController {
         model.addAttribute("totalPages", companies.getTotalPages());
 
         return "companies/company_list";
+    }
+
+    @GetMapping("/{id}")
+    public String getCompany(
+            @PathVariable Long id,
+            @RequestParam(defaultValue = "0") int page,
+            Model model
+    ) {
+
+        UserDto company = userService.findEmployerById(id);
+
+        Page<VacancyDto> vacancies = vacancyService.getVacanciesByUserId(id, page, 5);
+
+        model.addAttribute("company", company);
+        model.addAttribute("vacancies", vacancies.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", vacancies.getTotalPages());
+
+        return "companies/company_details";
     }
 }
