897e01c3a3762654771eae648f4e9f2e3ee66b98
Nurbolot Abilov <nurbolotbgk@gmail.com>
2026-08-25 17:44:44 +0600

    EmailServiceImpl some edited

---
+++ src/main/java/com/example/demo/service/impl/EmailServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.EmailService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+
+@Service
+@RequiredArgsConstructor
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String emailFrom;
+
+    @Override
+    public void sendEmail(String toEmail, String link)
+            throws MessagingException, UnsupportedEncodingException {
+
+        MimeMessage message = mailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        helper.setFrom(emailFrom, "Job Search Support");
+
+        helper.setTo(toEmail);
+
+        String subject = "Восстановление пароля";
+
+        String content =
+                "<p>Здравствуйте!</p>"
+                        + "<p>Вы запросили восстановление пароля.</p>"
+                        + "<p>Перейдите по ссылке:</p>"
+                        + "<p><a href=\"" + link + "\">"
+                        + "Изменить пароль"
+                        + "</a></p>"
+                        + "<p>Если вы не запрашивали восстановление пароля, "
+                        + "просто проигнорируйте это письмо.</p>";
+
+        helper.setSubject(subject);
+        helper.setText(content, true);
+        mailSender.send(message);
+    }
+}
